ff760fd7f3ab3900d4d26271fcd18b52997215dd
aratislamsafol <aratislamsafol@gmail.com>
2022-08-23 22:04:21 +0600

    Country Product Complete almost

--- js/main.js
+++ js/main.js
@@ -12,3 +12,41 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
     },
 });
+
+// Filter
+$(document).ready(function(){
+
+  $(".filter-button").click(function(){
+      var value = $(this).attr('data-filter');
+      
+      if(value == "all")
+      {
+          //$('.filter').removeClass('hidden');
+          $('.filter').show('1000');
+      }
+      else
+      {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+          $(".filter").not('.'+value).hide('3000');
+          $('.filter').filter('.'+value).show('3000');
+          
+      }
+  });
+  
+  if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
+// car
+function changeImage(element) {
+
+  var main_prodcut_image = document.getElementById('main_product_image');
+  main_prodcut_image.src = element.src;
+  
+
+}
+
